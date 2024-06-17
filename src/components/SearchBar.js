c2bfd59be39482e5b4cafc26b3b26ded4c9e5ade
AliveKiwi <prateekjais.dev@gmail.com>
2024-06-17 16:41:55 +0530

    95 style: styled the SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,7 +7,7 @@ const SearchBar = () => {
   return (
     <View style={styles.backgroundStyle}>
       {/* 93 added search icon */}
-      <Feather name="search" size={30} />
+      <Feather style={styles.iconStyle} name="search" size={30} />
       <TextInput placeholder="Search" style={styles.inputStyle} />
     </View>
   );
@@ -18,15 +18,23 @@ export default SearchBar;
 const styles = StyleSheet.create({
   // 92 makes SearchBar grey and rounded
   backgroundStyle: {
+    marginTop: 10,
     backgroundColor: '#f0eeee',
     height: 50,
     borderRadius: 5,
     marginHorizontal: 15,
     flexDirection: 'row',
   },
+  // 94 make TextInput expand to whole width
   inputStyle: {
     flex: 1,
     // borderColor: 'black', // 94 to check the width of TextInput
-    borderWidth: 1,
+    // borderWidth: 1,
+  },
+  // 94
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center', // 95 instead of alignItem
+    marginHorizontal: 15,
   },
 });
